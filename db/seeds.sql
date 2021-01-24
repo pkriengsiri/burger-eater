74abb56254b69c65b5777acd7d7dad6b90a6225c
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-24 16:28:58 -0500

    Added seeds to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Chile Relleno-You-Didn\'t Burger'),
+	   ('Don\'t You Four Cheddar Bout Me Burger (comes with four kinds of cheddar)'),
+       ('Chipotle Off the Old Block Burger'),
+       ('The Life of the Parsley Burger'),
+       ('These Collards Don\'t Run Burger');
